be8d8064c4959a1d6a00c7daee992edc8acae205
Fang <857559565@qq.com>
2022-10-07 13:51:20 +0800

    添加常亮类

---
+++ iot-common/iot-common-base/src/main/java/com/iot/common/constant/ValueConstant.java
@@ -0,0 +1,23 @@
+package com.iot.common.constant;
+
+/**
+ * @author ：FYQ
+ * @description：数据相关
+ * @date ：2022/10/7 13:49
+ */
+public interface ValueConstant {
+    /**
+     * 类型相关
+     */
+    interface Type {
+        String HEX = "hex";
+        String BYTE = "byte";
+        String SHORT = "short";
+        String INT = "int";
+        String LONG = "long";
+        String FLOAT = "float";
+        String DOUBLE = "double";
+        String BOOLEAN = "boolean";
+        String STRING = "string";
+    }
+}
